bae78fc81c6bd9577618d9736f58393e70c1b7e3
RomantsovS <romancov123@gmail.com>
2021-03-17 20:00:09 +0300

    ch 7.011 examples, exercises 7.1-7.3

---
+++ 07_Quick_sort/7.01_The_basic_algorithm/examples/ex_7_1/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <iomanip>
+
+#include "../../../../Mas.h"
+
+constexpr size_t M = 10, maxN = 10;
+
+template <typename Item>
+int partition(std::vector<Item> &a, int l, int r)
+{
+	int i = l - 1, j = r, v = a[r];
+
+	for (;;)
+	{
+		while (a[++i] < v)
+			;
+		while (v < a[--j])
+			if (j == l)
+				break;
+		if (i >= j)
+			break;
+
+		std::swap(a[i], a[j]);
+	}
+
+	std::swap(a[i], a[r]);
+	
+	return i;
+}
+
+template <typename Item>
+void quicksort(std::vector<Item> &a, int l, int r)
+{
+	if (r <= l)
+		return;
+
+	int i = partition(a, l, r);
+
+	quicksort(a, l, i - 1);
+	quicksort(a, i + 1, r);
+}
+
+int main()
+{
+	for (size_t i = 1; i <= 1; i++)
+	{
+		std::cout << "cnt = " << i << std::endl;
+		std::vector<int> mas(maxN);
+
+		for (size_t j = 0; j < maxN; ++j)
+		{
+			mas[j] = rand() % (M - 1);
+		}
+
+		mas.push_back(-1);
+
+		print_mas(mas, 3);
+
+		quicksort(mas, 0, mas.size() - 1);
+
+		print_mas(mas, 3);
+	}
+
+	return 0;
+}
