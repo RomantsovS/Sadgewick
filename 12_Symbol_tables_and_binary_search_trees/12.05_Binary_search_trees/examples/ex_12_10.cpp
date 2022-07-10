e7d0d0cde0bae14988f25563f96b26c43b1e8e14
RomantsovS <romantsov_s@rusklimat.ru>
2022-07-10 17:48:05 +0300

    ch 12.05

---
+++ 12_Symbol_tables_and_binary_search_trees/12.05_Binary_search_trees/examples/ex_12_10.cpp
@@ -0,0 +1,128 @@
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+static int maxKey = 1000;
+typedef int Key;
+
+class Item {
+   private:
+    Key keyval;
+    float info;
+
+   public:
+    Item() { keyval = maxKey; }
+    Item(Key key) { keyval = key; }
+    Key key() { return keyval; }
+    int null() { return keyval = maxKey; }
+    void rand() {
+        keyval = 1000 * ::rand() / RAND_MAX;
+        info = 1.0 * ::rand() / RAND_MAX;
+    }
+    int scan(istream& is = cin) { return !!(is >> keyval >> info); }
+    void show(ostream& os = cout) { os << keyval /*<< " " << info*/ << endl; }
+};
+
+ostream& operator<<(ostream& os, Item& x) {
+    x.show(os);
+    return os;
+}
+
+template <class Item, class Key>
+class ST {
+   private:
+    struct node {
+        Item item;
+        node *l, *r;
+        node(Item x) {
+            item = x;
+            l = 0;
+            r = 0;
+        }
+    };
+
+    typedef node* link;
+
+    link head;
+    Item nullItem;
+
+    Item searchR(link h, Key v) {
+        if (h == 0) return nullItem;
+        Key t = h->item.key();
+        if (v == t) return h->item;
+        if (v < t)
+            return searchR(h->l, v);
+        else
+            return searchR(h->r, v);
+    }
+
+    void showR(link h, ostream& os) {
+        if (h == 0) return;
+        showR(h->l, os);
+        h->item.show(os);
+        showR(h->r, os);
+    }
+
+   public:
+    ST([[maybe_unused]] int maxN) { head = 0; }
+    Item search(Key v) { return searchR(head, v); }
+
+    void insert(Item x) {
+        Key v = x.key();
+        if (head == 0) {
+            head = new node(x);
+            return;
+        }
+        link p = head;
+        for (link q = p; q != 0; p = q ? q : p) q = (v < q->item.key()) ? q->l : q->r;
+        if (v < p->item.key())
+            p->l = new node(x);
+        else
+            p->r = new node(x);
+    }
+
+    void show(ostream& os) { showR(head, os); }
+
+    template <typename T>
+    void print_node(T item, size_t offset) {
+        for (size_t i = 0; i != offset; ++i) cout << ' ';
+        cout << item;
+    }
+
+    void print(size_t offset = 2, size_t start_offset = 0) { print(head, offset, start_offset); }
+
+    void print(link curr_node, size_t offset = 2, size_t start_offset = 0) {
+        if (!curr_node) {
+            print_node('*', start_offset);
+            cout << endl;
+            return;
+        }
+
+        if (!curr_node->l && !curr_node->r) {
+            print_node(curr_node->item.key(), start_offset);
+            print_node('-', 0);
+            cout << endl;
+        } else {
+            print(curr_node->r, offset, start_offset + offset);
+
+            print_node(curr_node->item.key(), start_offset);
+            cout << endl;
+
+            print(curr_node->l, offset, start_offset + offset);
+        }
+    }
+};
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    ST<Item, Key> st(10);
+    st.insert({2});
+    st.insert({3});
+    st.insert({1});
+    st.insert({1});
+    st.insert({4});
+
+    st.show(cout);
+
+    cout << "ok\n";
+}
---
+++ 12_Symbol_tables_and_binary_search_trees/12.05_Binary_search_trees/examples/ex_12_8.cpp
@@ -0,0 +1,120 @@
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+static int maxKey = 1000;
+typedef int Key;
+
+class Item {
+   private:
+    Key keyval;
+    float info;
+
+   public:
+    Item() { keyval = maxKey; }
+    Item(Key key) { keyval = key; }
+    Key key() { return keyval; }
+    int null() { return keyval = maxKey; }
+    void rand() {
+        keyval = 1000 * ::rand() / RAND_MAX;
+        info = 1.0 * ::rand() / RAND_MAX;
+    }
+    int scan(istream& is = cin) { return !!(is >> keyval >> info); }
+    void show(ostream& os = cout) { os << keyval /*<< " " << info*/ << endl; }
+};
+
+ostream& operator<<(ostream& os, Item& x) {
+    x.show(os);
+    return os;
+}
+
+template <class Item, class Key>
+class ST {
+   private:
+    struct node {
+        Item item;
+        node *l, *r;
+        node(Item x) {
+            item = x;
+            l = 0;
+            r = 0;
+        }
+    };
+
+    typedef node* link;
+
+    link head;
+    Item nullItem;
+
+    Item searchR(link h, Key v) {
+        if (h == 0) return nullItem;
+        Key t = h->item.key();
+        if (v == t) return h->item;
+        if (v < t)
+            return searchR(h->l, v);
+        else
+            return searchR(h->r, v);
+    }
+
+    void insertR(link& h, Item x) {
+        if (h == 0) {
+            h = new node(x);
+            return;
+        }
+        if (x.key() < h->item.key())
+            insertR(h->l, x);
+        else
+            insertR(h->r, x);
+    }
+
+   public:
+    ST([[maybe_unused]] int maxN) { head = 0; }
+    Item search(Key v) { return searchR(head, v); }
+    void insert(Item x) { insertR(head, x); }
+
+    template <typename T>
+    void print_node(T item, size_t offset) {
+        for (size_t i = 0; i != offset; ++i) cout << ' ';
+        cout << item;
+    }
+
+    void print(size_t offset = 2, size_t start_offset = 0) { print(head, offset, start_offset); }
+
+    void print(link curr_node, size_t offset = 2, size_t start_offset = 0) {
+        if (!curr_node) {
+            print_node('*', start_offset);
+            cout << endl;
+            return;
+        }
+
+        if (!curr_node->l && !curr_node->r) {
+            print_node(curr_node->item.key(), start_offset);
+            print_node('-', 0);
+            cout << endl;
+        } else {
+            print(curr_node->r, offset, start_offset + offset);
+
+            print_node(curr_node->item.key(), start_offset);
+            cout << endl;
+
+            print(curr_node->l, offset, start_offset + offset);
+        }
+    }
+};
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    ST<Item, Key> st(10);
+    st.insert({2});
+    st.insert({3});
+    st.insert({1});
+    st.insert({1});
+    st.insert({4});
+
+    st.print();
+
+    assert(st.search(1).key() == 1);
+    assert(st.search(0).null());
+
+    cout << "ok\n";
+}
