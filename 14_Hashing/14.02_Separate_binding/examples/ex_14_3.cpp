db6b28998ed7a76a9efca3b20a2b60a3aa59567f
RomantsovS <romancov123@gmail.com>
2022-07-15 19:35:51 +0300

    ch 14.02

---
+++ 14_Hashing/14.02_Separate_binding/examples/ex_14_3.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+using Key = int;
+
+#include <Item.h>
+#include "HashTable.h"
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+    HashTable<Item, Key> ht(10);
+    ht.insert(1);
+    ht.insert(2);
+    ht.insert(3);
+
+    std::cout << ht.search(1).key() << std::endl;
+    std::cout << ht.search(2).key() << std::endl;
+    std::cout << ht.search(3).key() << std::endl;
+    std::cout << ht.search(4).key() << std::endl;
+
+    std::cout << "ok\n";
+}
